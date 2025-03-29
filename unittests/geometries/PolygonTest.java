4c98bf3606e5e99127839c0c577b889cde1cd03e
yokheved <yokheved.spr@gmail.com>
2025-03-30 00:01:02 +0300

    unittest init

--- unittests/geometries/PolygonTest.java
+++ unittests/geometries/PolygonTest.java
@@ -1,4 +1,4 @@
-package unittests.geometries;
+package geometries;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +14,7 @@ import primitives.*;
  * Testing Polygons
  * @author Dan
  */
-class PolygonTests {
+class PolygonTest {
    /**
     * Delta value for accuracy when comparing the numbers of type 'double' in
     * assertEquals
@@ -35,36 +35,52 @@ class PolygonTests {
 
       // TC02: Wrong vertices order
       assertThrows(IllegalArgumentException.class, //
-                   () -> new Polygon(new Point(0, 0, 1), new Point(0, 1, 0), new Point(1, 0, 0), new Point(-1, 1, 1)), //
+                   () -> new Polygon(new Point(0, 0, 1),
+                           new Point(0, 1, 0),
+                           new Point(1, 0, 0),
+                           new Point(-1, 1, 1)), //
                    "Constructed a polygon with wrong order of vertices");
 
       // TC03: Not in the same plane
       assertThrows(IllegalArgumentException.class, //
-                   () -> new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 2, 2)), //
+                   () -> new Polygon(new Point(0, 0, 1),
+                           new Point(1, 0, 0),
+                           new Point(0, 1, 0),
+                           new Point(0, 2, 2)), //
                    "Constructed a polygon with vertices that are not in the same plane");
 
       // TC04: Concave quadrangular
       assertThrows(IllegalArgumentException.class, //
-                   () -> new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0),
-                                     new Point(0.5, 0.25, 0.5)), //
+                   () -> new Polygon(new Point(0, 0, 1),
+                           new Point(1, 0, 0),
+                           new Point(0, 1, 0),
+                           new Point(0.5, 0.25, 0.5)), //
                    "Constructed a concave polygon");
 
       // =============== Boundary Values Tests ==================
 
       // TC10: Vertex on a side of a quadrangular
       assertThrows(IllegalArgumentException.class, //
-                   () -> new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0),
-                                     new Point(0, 0.5, 0.5)),
+                   () -> new Polygon(new Point(0, 0, 1),
+                           new Point(1, 0, 0),
+                           new Point(0, 1, 0),
+                           new Point(0, 0.5, 0.5)),
                    "Constructed a polygon with vertix on a side");
 
       // TC11: Last point = first point
       assertThrows(IllegalArgumentException.class, //
-                   () -> new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 0, 1)),
+                   () -> new Polygon(new Point(0, 0, 1),
+                           new Point(1, 0, 0),
+                           new Point(0, 1, 0),
+                           new Point(0, 0, 1)),
                    "Constructed a polygon with vertice on a side");
 
       // TC12: Co-located points
       assertThrows(IllegalArgumentException.class, //
-                   () -> new Polygon(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0), new Point(0, 1, 0)),
+                   () -> new Polygon(new Point(0, 0, 1),
+                           new Point(1, 0, 0),
+                           new Point(0, 1, 0),
+                           new Point(0, 1, 0)),
                    "Constructed a polygon with vertice on a side");
 
    }
@@ -75,7 +91,11 @@ class PolygonTests {
       // ============ Equivalence Partitions Tests ==============
       // TC01: There is a simple single test here - using a quad
       Point[] pts =
-         { new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0), new Point(-1, 1, 1) };
+         {
+                 new Point(0, 0, 1),
+                 new Point(1, 0, 0),
+                 new Point(0, 1, 0),
+                 new Point(-1, 1, 1) };
       Polygon pol = new Polygon(pts);
       // ensure there are no exceptions
       assertDoesNotThrow(() -> pol.getNormal(new Point(0, 0, 1)), "");
