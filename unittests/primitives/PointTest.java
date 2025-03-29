fa23c9b0f394722c79d71b3a64c431198eed5caf
yokheved <yokheved.spr@gmail.com>
2025-03-30 00:00:51 +0300

    unittest init

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,52 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    /**
+     * test method for{@link Point#add(Vector)}
+     */
+    @Test
+    void testAdd() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+        fail("Not yet implemented");
+    }
+
+    /**
+     * test method for{@link Point#{METHOD_NAME}(${PARAMETERS})}
+     */
+    @Test
+    void testSubtract() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+        fail("Not yet implemented");
+    }
+
+    /**
+     * test method for{@link Point#{METHOD_NAME}(${PARAMETERS})}
+     */
+    @Test
+    void testDistanceSquared() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+        fail("Not yet implemented");
+    }
+
+    /**
+     * test method for{@link Point#{METHOD_NAME}(${PARAMETERS})}
+     */
+    @Test
+    void testDistance() {
+        // ============ Equivalence Partitions Tests ==============
+
+        // =============== Boundary Values Tests ==================
+        fail("Not yet implemented");
+    }
+}
