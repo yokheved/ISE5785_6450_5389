8021c735e98e80b0601aafee4978d632e630f528
yokheved <yokheved.spr@gmail.com>
2025-04-06 14:00:31 +0300

    unittest point: add and subtract written, add workes.

--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -11,25 +11,33 @@ class PointTest {
      */
     @Test
     void testAdd() {
+        Point p1 = new Point(1, 2, 3);
+        Vector v2 = new Vector(0, 2 , 9);
         // ============ Equivalence Partitions Tests ==============
-
+        //TC1: all rational numbers
+        Point added = new Point(1+0, 2+2, 3+9);
+        assertEquals(added, p1.add(v2), "adding vector to point was unexpected");
         // =============== Boundary Values Tests ==================
-        fail("Not yet implemented");
+        //none
     }
 
     /**
-     * test method for{@link Point#{METHOD_NAME}(${PARAMETERS})}
+     * test method for{@link Point#subtract(Point)}
      */
     @Test
     void testSubtract() {
+        Point p1 = new Point(3,5,-7);
+        Point p2 = new Point(5, 0, 2);
         // ============ Equivalence Partitions Tests ==============
-
+        //TC1: all rational numbers
+        Vector subtracted = new Vector(3-5, 5-0, -7-2);
+        assertEquals(subtracted, p1.subtract(p2), "subtracting point with point was unexpected");
         // =============== Boundary Values Tests ==================
         fail("Not yet implemented");
     }
 
     /**
-     * test method for{@link Point#{METHOD_NAME}(${PARAMETERS})}
+     * test method for{@link Point#distanceSquared(Point)}
      */
     @Test
     void testDistanceSquared() {
@@ -40,7 +48,7 @@ class PointTest {
     }
 
     /**
-     * test method for{@link Point#{METHOD_NAME}(${PARAMETERS})}
+     * test method for{@link Point#distance(Point)}
      */
     @Test
     void testDistance() {
