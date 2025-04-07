2ad638d539d7f619eb5f0c3490fc385c67e8ba53
yokheved <yokheved.spr@gmail.com>
2025-04-08 00:48:48 +0300

    unittest point: all working

--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -3,6 +3,7 @@ package primitives;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static primitives.Util.isZero;
 
 class PointTest {
 
@@ -33,7 +34,7 @@ class PointTest {
         Vector subtracted = new Vector(3-5, 5-0, -7-2);
         assertEquals(subtracted, p1.subtract(p2), "subtracting point with point was unexpected");
         // =============== Boundary Values Tests ==================
-        fail("Not yet implemented");
+        //--none
     }
 
     /**
@@ -41,10 +42,16 @@ class PointTest {
      */
     @Test
     void testDistanceSquared() {
+        Point p1 = new Point(3,5,-7);
+        Point p2 = new Point(5, 0, 2);
         // ============ Equivalence Partitions Tests ==============
-
+        //TC1: all rational coordinates
+        double dissqrd = (3-5)*(3-5)+(5-0)*(5-0)+(-7-2)*(-7-2);
+        assertTrue(isZero(dissqrd - p1.distanceSquared(p2)));
         // =============== Boundary Values Tests ==================
-        fail("Not yet implemented");
+        //TC2: the points are the same
+        assertTrue(isZero(p1.distanceSquared(p1)));
+
     }
 
     /**
@@ -52,9 +59,14 @@ class PointTest {
      */
     @Test
     void testDistance() {
+        Point p1 = new Point(3,5,-7);
+        Point p2 = new Point(5, 0, 2);
         // ============ Equivalence Partitions Tests ==============
-
+        //TC1: all rational coordinates
+        double dis = Math.sqrt((3-5)*(3-5)+(5-0)*(5-0)+(-7-2)*(-7-2));
+        assertTrue(isZero(dis - p1.distance(p2)));
         // =============== Boundary Values Tests ==================
-        fail("Not yet implemented");
+        //TC2: the points are the same
+        assertTrue(isZero(p1.distance(p1)));
     }
 }
