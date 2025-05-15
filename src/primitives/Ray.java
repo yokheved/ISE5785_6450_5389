0bab89bf3d1c91f9ceddfc00c579bd4b172b1150
yokheved <yokheved.spr@gmail.com>
2025-05-15 14:19:21 +0300

    for merge

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -10,10 +10,14 @@ import java.util.Objects;
  */
 public class Ray {
 
-    /** The head (origin) of the ray */
+    /**
+     * The head (origin) of the ray
+     */
     private Point head;
 
-    /** The direction of the ray */
+    /**
+     * The direction of the ray
+     */
     private Vector direction;
 
     public Ray(Point head, Vector direction) {
@@ -50,7 +54,7 @@ public class Ray {
         return direction;
     }
 
-    public Point getPoint(double t){
+    public Point getPoint(double t) {
         return getHead().add(getDirection().scale(t));
     }
 }
